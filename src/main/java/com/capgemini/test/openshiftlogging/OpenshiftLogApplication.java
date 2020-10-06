8f98dcd1f868cf6f76f012c6cf539c80bb816539
Alexander Friese <af944580@googlemail.com>
2020-10-06 08:10:42 +0200

    renaming test

--- src/main/java/com/capgemini/test/openshiftlogging/OpenshiftLogApplication.java
+++ src/main/java/com/capgemini/test/openshiftlogging/OpenshiftLogApplication.java
@@ -7,15 +7,15 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.web.WebApplicationInitializer;
 
 @SpringBootApplication
-public class OpenshiftLoggingApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
+public class OpenshiftLogApplication extends SpringBootServletInitializer implements WebApplicationInitializer {
 
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(OpenshiftLoggingApplication.class);
+        return application.sources(OpenshiftLogApplication.class);
     }
 
     public static void main(String[] args) {
-        SpringApplication.run(OpenshiftLoggingApplication.class, args);
+        SpringApplication.run(OpenshiftLogApplication.class, args);
     }
 
 }
